f6fc23ed7cf34956ccc873bf2178da5f99c6746b
talm2 <talmilo@gmail.com>
2025-12-30 19:22:59 +0200

    chore: Save project state and monitoring config

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,20 +1,23 @@
-    terraform {
-      required_version = ">= 1.0"
+terraform {
+  required_version = ">= 1.0"
 
-      required_providers {
-        google = {
-          source  = "hashicorp/google"
-          version = "~> 5.0"
-        }
-      }
-
-    backend "gcs" {
-      bucket = "sre-portfolio-project-481507-tfstate"
-      prefix = "terraform/state"
+  required_providers {
+    # The Google Provider allows Terraform to interact with GCP APIs (Compute, GKE, etc.)
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
     }
-   }
+  }
 
-    provider "google" {
-      project = var.project_id
-      region  = var.region
-    }
+  # Configure the Backend to store the state file in a GCS bucket.
+  # This allows for shared state and locking, essential for teams.
+  backend "gcs" {
+    bucket = "sre-portfolio-project-481507-tfstate"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
